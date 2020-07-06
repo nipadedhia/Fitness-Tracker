b7d8fb13c33ed71b7880f8b6816a425e7781432d
Nipa Dedhia <nipa.dedhia@gmail.com>
2020-07-06 13:34:00 -0500

    twik code on line 12

--- public/js/api.js
+++ public/js/api.js
@@ -4,11 +4,12 @@ const API = {
     try {
       res = await fetch("/api/workouts");
     } catch (err) {
-      console.log(err)
+      console.log(err);
     }
     const json = await res.json();
 
-    return json[json.length - 1];
+    // return json[json.length - 1];
+    return json;
   },
   async addExercise(data) {
     const id = location.search.split("=")[1];
@@ -16,7 +17,7 @@ const API = {
     const res = await fetch("/api/workouts/" + id, {
       method: "PUT",
       headers: { "Content-Type": "application/json" },
-      body: JSON.stringify(data)
+      body: JSON.stringify(data),
     });
 
     const json = await res.json();
@@ -27,7 +28,7 @@ const API = {
     const res = await fetch("/api/workouts", {
       method: "POST",
       body: JSON.stringify(data),
-      headers: { "Content-Type": "application/json" }
+      headers: { "Content-Type": "application/json" },
     });
 
     const json = await res.json();
